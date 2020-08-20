8f82cb052d755c9d25dd9ffb07241059b3cd0ff5
Emily Phee <ephee@uci.edu>
2020-08-20 15:22:15 -0700

    Delete blank lines

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ let enteredNum = document.getElementById('guess-input');
 const submitButton = document.getElementById('guess-value');
 let statusMessage = document.getElementsByClassName('message');
 let p = document.querySelector('.message');
+
 const maxPlayNum = 3;
-let playCount = 0;
 let chances = 3;
+let playCount = 0;
 
 
 
@@ -13,7 +14,6 @@ let randomNum = Math.round(Math.random() * 10);
 submitButton.addEventListener('click', guessNumValidator);
 
 function guessNumValidator(e) {
-
   // Check if the target value is PLAY AGAIN or SUBMIT
   if (e.target.value === 'PLAY AGAIN') {
     enteredNum.disabled = false;
@@ -21,22 +21,15 @@ function guessNumValidator(e) {
     chances = 3;
 
     statusMessage[0].innerHTML = '';
-
   } else {
     if (playCount < maxPlayNum) {
       if (Number(enteredNum.value) === randomNum) {
-
-  
         playCount = 0;
         randomNum = Math.round(Math.random() * 10);
 
         // Convert message color to green
         p.className = 'message correct-answer';
-
         enteredNum.id = 'guess-input correct-answer';
-
-        
-
 
         statusMessage[0].innerHTML = `${Number(enteredNum.value)} is correct, YOU WIN!`
         enteredNum.disabled = true;
@@ -52,15 +45,10 @@ function guessNumValidator(e) {
 
         statusMessage[0].innerHTML = `${Number(enteredNum.value)} is not correct, ${chances} guesses left`;
   
-   
         if (playCount === maxPlayNum) {
-  
-          
           playCount = 0;
           randomNum = Math.round(Math.random() * 10);
-  
           enteredNum.disabled = true;
-
           statusMessage[0].innerHTML = `Sorry, game over, the correct answer was ${randomNum}`;
           submitButton.value = 'PLAY AGAIN';
 
@@ -68,7 +56,6 @@ function guessNumValidator(e) {
       }
     } 
   }
-
   // Clean the input field after SUBMIT button clicked
   enteredNum.value = '';
 
